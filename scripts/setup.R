8398df2b23a39b9d02a2d2ef4a7ec631f1c6ce6b
JM0327 <jiaming.huang@unil.ch>
2022-04-29 23:53:39 +0200

    hide some setup for figure output

--- scripts/setup.R
+++ scripts/setup.R
@@ -35,14 +35,15 @@ opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
   # cache = TRUE,
-  fig.retina = 0.8, # figures are either vectors or 300 dpi diagrams
-  dpi = 300,
-  out.width = "70%",
+  # fig.retina = 0.8, # figures are either vectors or 300 dpi diagrams
+  # dpi = 300,
+  # out.width = "70%",
   fig.align = "center",
-  fig.width = 6,
-  fig.asp = 0.618,
+  # fig.width = 6,
+  # fig.asp = 0.618,
   fig.show = "hold",
   message = FALSE,
-  echo = FALSE
+  echo = FALSE,
+  warning = FALSE
 )
 
